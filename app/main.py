545c1ef0c2866a019627e31fd1de3c5b5b6c4597
ShaiThur <sartur823@gmail.com>
2025-10-12 15:45:21 +0300

    Changed llm, disabled CORS

--- app/main.py
+++ app/main.py
@@ -3,10 +3,11 @@ import subprocess
 
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from logging.config import dictConfig
 
-from common import LOGGING_CONFIG, CommonConstants, DbConstants
+from common import LOGGING_CONFIG, DbConstants, CommonConstants
 from routers import voice_router, llm_router
 from middleware import ExceptionMiddleware
 from routers import history_router
@@ -36,7 +37,8 @@ app = FastAPI()
 
 # noinspection PyTypeChecker
 app.add_middleware(ExceptionMiddleware)
-
+# noinspection PyTypeChecker
+app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"] )
 app.include_router(voice_router)
 app.include_router(llm_router)
 app.include_router(history_router)
@@ -46,4 +48,4 @@ if DbConstants.PYWAY_ENABLED:
     run_migrations()
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8080, log_config=LOGGING_CONFIG)
+    uvicorn.run(app, host="0.0.0.0", port=8080, log_config=LOGGING_CONFIG, log_level=CommonConstants.LOG_LEVEL)
